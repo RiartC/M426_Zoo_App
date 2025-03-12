9441ed5b3df86365d588e4d8316b9717e6ec7f53
Fabian0tt <fa.ott@bluewin.ch>
2025-03-12 09:54:51 +0100

    impressum anfang

---
+++ M426_Zoo_App-main/src/impressum.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Impressum = () => {
+    return (
+        <div className="max-w-2xl mx-auto p-6 bg-white shadow-lg rounded-2xl">
+            <h1 className="text-2xl font-bold mb-4">Impressum</h1>
+            <p><strong>Angaben gemäß § 5 TMG:</strong></p>
+            <p>Firmenname: <strong>Zoo</strong></p>
+            <p>Land: Schweiz</p>
+
+            <h2 className="text-xl font-semibold mt-4">Kontakt</h2>
+            <p>Telefon: +49 123 456789</p>
+            <p>E-Mail: riart.cekaj@stud.kbw.ch, fabian.ott1@stud.kbw.ch</p>
+        </div>
+    );
+};
+
+export default Impressum;
