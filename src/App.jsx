439e6d4f140be42b27f473dd2a37f7a852274e57
RiartC <cekajriart@gmail.com>
2025-03-19 07:44:14 +0100

    startseite

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
 
 function App() {
-
-
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-green-100 p-6">
+            <h1 className="text-3xl font-bold text-green-700">Willkommen im Zoo</h1>
+            <p className="mt-4 text-lg text-gray-700">Erlebe die faszinierende Welt der Tiere!</p>
+        </div>
+    );
 }
 
-export default App
+export default App;
